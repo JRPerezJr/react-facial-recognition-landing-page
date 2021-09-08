085792d4e24147b5047a809c7d3ce15180238532
Juan <pront23@gmail.com>
2021-09-08 21:25:07 +0900

    added routes and App classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Header } from './components/header/header';
+import { Route, Switch } from 'react-router-dom';
+import { HomePage } from './pages/homepage/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App font-Poppins">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
