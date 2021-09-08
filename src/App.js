816d09347cbea561a64b4506f6b465eb24b59f32
Juan <pront23@gmail.com>
2021-09-09 02:09:02 +0900

    added base routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { Header } from './components/header/header';
 import { Route, Switch } from 'react-router-dom';
 import { HomePage } from './pages/homepage/home';
+import { FeaturesPage } from './pages/features-page/features-page';
+import { PricingPage } from './pages/pricing-page/pricing-page';
+import { AboutPage } from './pages/about/about-page';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/features" component={FeaturesPage} />
+        <Route path="/pricing" component={PricingPage} />
+        <Route path="/about" component={AboutPage} />
       </Switch>
     </div>
   );
