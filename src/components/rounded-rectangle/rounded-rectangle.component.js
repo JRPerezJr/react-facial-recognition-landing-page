619a7e75103cf19723b7473f1aa9350d9e006a90
Juan <pront23@gmail.com>
2021-09-08 21:23:14 +0900

    added rounded rectangle component

---
+++ src/components/rounded-rectangle/rounded-rectangle.component.js
@@ -0,0 +1,5 @@
+export const RoundedRectangleComponent = () => {
+  return (
+    <div className="hidden md:block overflow-hidden bg-light-blue rounded-l-full absolute h-80 w-2/4 top-32 right-0 lg:-bottom-28 lg:-right-26" />
+  );
+};
