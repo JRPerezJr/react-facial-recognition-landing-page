51a71cbd08e2ac982b125396edbdbc080c0314e5
Juan <pront23@gmail.com>
2021-09-08 21:20:16 +0900

    Extended theme with custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'deep-blue': '#02001b',
+        'light-blue': '#2214c8',
+        primary: '#ffffff',
+        secondary: '#000000',
+        'btn-red': '#D43A25',
+      },
+    },
+    fontFamily: {
+      Poppins: [
+        "-apple-system, BlinkMacSystemFont,'Poppins','Segoe UI', 'Roboto', 'Oxygen',\n" +
+          "'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',\n" +
+          'sans-serif',
+      ],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
+    },
   },
   variants: {
     extend: {},
