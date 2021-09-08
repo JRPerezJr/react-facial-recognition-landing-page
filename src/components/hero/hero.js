0e5ca33a0685fe5aea830c90bad57cdfc7a0e20e
Juan <pront23@gmail.com>
2021-09-09 02:10:58 +0900

    added rounded rectangle component

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,5 +1,6 @@
 import { HeroImageComponent } from '../hero-image/hero-image.component';
 import { RoundedRectangleComponent } from '../rounded-rectangle/rounded-rectangle.component';
+import { ButtonComponent } from '../button-component/button.component';
 
 export const HeroComponent = () => {
   return (
@@ -7,24 +8,28 @@ export const HeroComponent = () => {
       <div className="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28">
         <div className="flex flex-1 flex-col items-center lg:items-start">
           <h2 className="text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
-            A simple facial recognition web application
+            A simple facial recognition application
           </h2>
           <p className="text-lg text-center lg:text-left mb-6">
             A clean and simple interface to upload your photo links and detect
             faces. Try it for free.
           </p>
           <div className="flex justify-center flex-wrap gap-6">
-            <button
-              type="button"
-              className="btn text-primary btn-black hover:bg-primary hover:text-secondary"
-            >
-              Try it now
-            </button>
+            <ButtonComponent
+              twClass={
+                'btn text-primary btn-black hover:bg-primary hover:text-secondary'
+              }
+              text={'Try it now'}
+            />
           </div>
         </div>
         <HeroImageComponent />
       </div>
-      <RoundedRectangleComponent />
+      <RoundedRectangleComponent
+        twClass={
+          'hidden md:block overflow-hidden bg-light-blue rounded-l-full absolute h-80 w-2/4 top-32 right-0 lg:-bottom-28 lg:-right-36'
+        }
+      />
     </section>
   );
 };
