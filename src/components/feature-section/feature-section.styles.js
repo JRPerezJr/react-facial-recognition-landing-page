26197103b3110cc9dcf113d805cf555c42a29f6d
Juan <pront23@gmail.com>
2021-09-09 02:08:13 +0900

    removed feature section into on component

---
+++ src/components/feature-section/feature-section.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const FeatureContainer = styled.section.attrs({
+  className: 'bg-light-white py-20 mt-20 lg:mt-60',
+})``;
+
+export const HeadingContainer = styled.div.attrs({
+  className: 'sm:w-3/4 lg:w-5/12 mx-auto px-2',
+})``;
+export const StyledHeading = styled.h1.attrs({
+  className: 'text-3xl text-center text-light-blue',
+})``;
+
+export const StyledParagraph = styled.p.attrs({
+  className: 'text-center text-grey mt-4',
+})``;
