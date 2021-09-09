007f11831cf83dd958d03ab2cefcbea459c18cbd
Juan <pront23@gmail.com>
2021-09-10 00:34:42 +0900

    Added Download component

--- src/pages/homepage/home.js
+++ src/pages/homepage/home.js
@@ -1,11 +1,13 @@
 import { HeroComponent } from '../../components/hero/hero';
 import { FeatureSection } from '../../components/feature-section/feature-section.component';
+import { DownloadComponent } from '../../components/download/download.component';
 
 export const HomePage = () => {
   return (
     <>
       <HeroComponent />
       <FeatureSection />
+      <DownloadComponent />
     </>
   );
 };
