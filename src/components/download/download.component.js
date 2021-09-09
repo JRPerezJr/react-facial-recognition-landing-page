c6c811827e2e9dae8b554a5b4c8f7b4436e3e004
Juan <pront23@gmail.com>
2021-09-10 00:35:18 +0900

    Added Download component and styles

---
+++ src/components/download/download.component.js
@@ -0,0 +1,22 @@
+import { StyledDownloadSection } from './download.styles';
+import {
+  HeadingContainer,
+  StyledHeading,
+  StyledParagraph,
+} from '../feature-section/feature-section.styles';
+import { CardComponent } from '../card-component/card.component';
+
+export const DownloadComponent = () => {
+  return (
+    <StyledDownloadSection>
+      <HeadingContainer>
+        <StyledHeading>Download the application</StyledHeading>
+        <StyledParagraph>
+          We've got more apps in the pipeline. Please let us know if you're
+          interested in more new and exciting features.
+        </StyledParagraph>
+      </HeadingContainer>
+      <CardComponent />
+    </StyledDownloadSection>
+  );
+};
---
+++ src/components/download/download.styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledDownloadSection = styled.section.attrs({
+  className: 'py-20 mt-20',
+})``;
