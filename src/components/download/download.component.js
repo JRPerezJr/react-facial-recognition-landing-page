a0814be377639d667d026b82b8db45bc02b71ce0
Juan <pront23@gmail.com>
2021-09-11 17:48:27 +0900

    refactored to use heading component

--- src/components/download/download.component.js
+++ src/components/download/download.component.js
@@ -1,21 +1,17 @@
 import { StyledDownloadSection } from './download.styles';
-import {
-  HeadingContainer,
-  StyledHeading,
-  StyledParagraph,
-} from '../feature-section/feature-section.styles';
+
 import { CardComponent } from '../card-component/card.component';
+import { HeadingComponent } from '../heading-component/heading.component';
 
 export const DownloadComponent = () => {
   return (
     <StyledDownloadSection>
-      <HeadingContainer>
-        <StyledHeading>Download the application</StyledHeading>
-        <StyledParagraph>
-          We've got more apps in the pipeline. Please let us know if you're
-          interested in adding more new and exciting features.
-        </StyledParagraph>
-      </HeadingContainer>
+      {/*Heading*/}
+      <HeadingComponent
+        heading="Download the application"
+        paragraph="We've got more apps in the pipeline. Please let us know if you're
+          interested in adding more new and exciting features."
+      />
       {/*Cards*/}
       <CardComponent />
     </StyledDownloadSection>
--- src/components/feature-section/feature-section.component.js
+++ src/components/feature-section/feature-section.component.js
@@ -1,25 +1,19 @@
 import { featureItems } from '../../stores/features';
 
-import {
-  FeatureContainer,
-  HeadingContainer,
-  StyledHeading,
-  StyledParagraph,
-} from './feature-section.styles';
+import { FeatureContainer } from './feature-section.styles';
 
 import { FeatureItem } from '../feature-item/feature-item';
+import { HeadingComponent } from '../heading-component/heading.component';
 
 export const FeatureSection = () => {
   return (
     <FeatureContainer>
       {/*Heading*/}
-      <HeadingContainer>
-        <StyledHeading>Features</StyledHeading>
-        <StyledParagraph>
-          Our aim is to make it quick and easy for you to use our facial
-          recognition feature to match faces.
-        </StyledParagraph>
-      </HeadingContainer>
+      <HeadingComponent
+        heading="Features"
+        paragraph="Our aim is to make it quick and easy for you to use our facial
+          recognition feature to match faces."
+      />
       {/*Feature 1*/}
       {featureItems.map(featureItem => (
         <FeatureItem
